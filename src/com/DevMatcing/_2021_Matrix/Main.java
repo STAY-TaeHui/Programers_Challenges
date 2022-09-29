bac2b4e3b93ba366e63fe7a0627d3c057b2e6b6d
서태희 <masta95@naver.com>
2022-09-29 13:17:52 +0900

    Dev-Matching 2021 - Turn Right Maxtrix

---
+++ src/com/DevMatcing/_2021_Matrix/Main.java
@@ -0,0 +1,13 @@
+package com.DevMatcing._2021_Matrix;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        //6,6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
+        Solution s = new Solution();
+        s.solution(6,6,new int[][]{{2,2,5,4},{3,3,6,6},{5,1,6,3}});
+
+    }
+}
